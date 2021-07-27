4a2b95b1eed646387f380c3ebd5a33ac8f82316d
Édini Marques Zanlucas <ezanlucas@PSG.LOCAL>
2021-07-27 15:24:47 -0400

    final do modulo 6

--- clientes-angular/src/app/app.module.ts
+++ clientes-angular/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent} from './hello/hello.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ clientes-angular/src/app/hello/hello.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core'
+
+@Component({
+  selector:'hello',
+  template: `
+  <h1>Olá Humanidade, eu venho em paz dominar o seu pequeno planeta</h1>
+  `
+})
+export class HelloComponent{
+
+}
